fa3e603d5b5f7475e3d79e811a3536165a1d3751
BrianKvin <briankvin@gmail.com>
2023-01-06 12:43:50 -0800

    Write a class Rectangle that defines a rectangle by: (based on 4-rectangle.py)

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -74,6 +74,7 @@ class Rectangle:
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
+
     def __repr__(self):
         """Return a string representation of the rectangle"""
         return f"Rectangle({self.__width}, {self.__height})"
