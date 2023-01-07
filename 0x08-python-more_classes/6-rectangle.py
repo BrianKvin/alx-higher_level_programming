726ea38bed682a22a633672ce2ecf1d2e15aed90
BrianKvin <briankvin@gmail.com>
2023-01-06 23:35:29 -0800

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -77,7 +77,7 @@ class Rectangle:
 
     def __repr__(self):
         """Return a string representation of the rectangle"""
-        return f"Rectangle({self.__width}, {self.__height})"
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
         """Return a message, when an instance of Rectangle is deleted."""
