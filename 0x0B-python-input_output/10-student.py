959da261666c4972647591adfc3ccdff3b9ba958
BrianKvin <briankvin@gmail.com>
2023-01-13 23:36:58 -0800

    Student to JSON with filter

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,7 +3,7 @@
 
 """A class student that defines a student based on 9-student.py"""
 class Student:
-    def __init__(self first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
