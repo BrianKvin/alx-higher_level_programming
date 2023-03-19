1d6732844d3084e3835b71d7bbc4681b3a64971b
BrianKvin <briankvin@gmail.com>
2023-03-19 22:01:56 +0300

    Select states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+This script lists all states from the database 'hbtn_0e_0_usa'
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    Access to the database amd get the states
+    from the database
+    """
+    db = MySQLdb.connect(host="localhost",
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    data = cursor.fetchall()
+
+    for row in data:
+        print(row)
+
+    cursor.close()
+    db.close()
