707d5131c3f405d0734e670c83a0b5910f69e5ef
BrianKvin <briankvin@gmail.com>
2023-03-20 03:14:20 -0700

    Contains 'a'

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -7,17 +7,17 @@ if __name__ == "__main__":
     import sys
     from model_state import Base, State
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
+    from sqlalchemy.orm import sessionmaker
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'\
             .format(sys.argv[1], sys.argv[2],\
             sys.argv[3]), pool_pre_ping=True)
 
-    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    session = Session(engine)
     for state in session.query(State)\
-            .filter(State.name.like('%%'))\
+            .filter(State.name.contains('a'))\
             .order_by(State.id):
         print("{}: {}".format(state.id, state.name))
     session.close()
