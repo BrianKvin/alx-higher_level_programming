51591639b118021776143c2ed465515e7c613008
BrianKvin <briankvin@gmail.com>
2023-03-23 01:12:40 -0700

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,11 +10,13 @@ import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1],
-            passwd=sys.argv[2],
-            db=sys.argv[3],
-            host='localhost',
-            port=3306)
+
+    db = MySQLdb.connect(
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        host='localhost',
+                        port=3306)
 
     cur = db.cursor()
 
