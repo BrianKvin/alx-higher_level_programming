a3fb59175fe00d21ff76f3d75986fcf689a52590
Bkelvin <briankvin@gmail.com>
2023-04-26 00:29:06 +0300

    Edits

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,12 +5,20 @@
  * the content of the file must be written in utf-8
  * if an error occurred during writing, print the error object
  * */
+//const fs = require('fs');
+//const filePath = process.argv[2];
+//const writeData = process.argv[3];
+
+//fs.writeFile(filePath, writeData, 'utf-8', (error) => {
+//    if (error) {
+//        console.log(error);
+//    }
+//});
+
 const fs = require('fs');
 const filePath = process.argv[2];
 const writeData = process.argv[3];
 
 fs.writeFile(filePath, writeData, 'utf-8', (error) => {
-    if (error) {
-        console.log(error);
-    }
+  if (error) { console.log(error); }
 });
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 // Displays the status of a GET request
-const request = require('request');
-const url = process.argv[2];
+//const request = require('request');
+//const url = process.argv[2];
 
-request.get(url).on('response', function (response) => {
-	console.log(`code: ${response.statusCode}`);
+//request.get(url).on('response', function (response) => {
+//	console.log(`code: ${response.statusCode}`);
+//});
+
+const request = require('request');
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
