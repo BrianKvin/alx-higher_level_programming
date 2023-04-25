03ccc0ef5500e6737c5931f538d3f25c199f100b
Bkelvin <briankvin@gmail.com>
2023-04-25 23:52:16 +0300

    Writeme

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+ * writes a string to a file
+ * the second argumnet is the string to write
+ * the content of the file must be written in utf-8
+ * if an error occurred during writing, print the error object
+ * */
+const fs = required('fs');
+const filepath = process.argv[2];
+const writeData = process.argv[3];
+
+fs.writeFile(filePath, writeData, 'utf-8', (error) => {
+        if (error) {
+                console.log(error);
+        }
+});
