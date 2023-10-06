a5d7c7079fee904eff7360163292c5cd0bfd8078
BrianKvin <briankvin@gmail.com>
2023-10-06 22:15:43 +0300

    edited 5 and  7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,19 @@
 #!/usr/bin/node
-exports.nbOccurrences = function (list, searchElement) {
-	return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
+//exports.nbOccurrences = function (list, searchElement) {
+//	return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
+// };
+
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  list.map(
+    (x) => {
+      if (x === searchElement) {
+        count += 1;
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  );
+  return count;
 };
